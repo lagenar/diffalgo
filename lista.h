2a3e18d08ff49ee8359c1971453588f47a8131cb
lucas <lucas@lucas-desktop>
2009-05-16 10:22:27 -0300

    implementacion de una lista. Por ahora sin iterador.

---
+++ lista.h
@@ -0,0 +1,19 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+template <class T>
+class Lista {
+  struct nodo_lista{
+    T elem;
+    struct nodo_lista* sig;
+  } cabeza, final;
+
+  void _eliminarLista();
+  
+ public:
+  Lista();
+  ~Lista();
+  void insertarFinal(T elem);
+};
+
+#endif
