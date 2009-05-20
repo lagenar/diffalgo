2c6a092048127ad2366ff1c976191e00f2a27821
mmatos <mmatos@ubuntu.ubuntu-domain>
2009-05-19 21:17:36 -0300

    Se modific? la clase diferencial para que contemple el caso de la subsecuencia vac?a

--- Diferencial.cpp
+++ Diferencial.cpp
@@ -32,30 +32,38 @@ void Diferencial::crearCambios()
 {
     int i(1), j(1);
     IteradorLista<parInt> it(&subsecuencia);
-    if (i < it.elemActual().primero) {
-        Cambios.insertarFinal(new CambioEliminar(it.elemActual().segundo,1,it.elemActual().primero-1));
-        i=it.elemActual().primero;
-    }
-    if (j < it.elemActual().segundo) {
-        Cambios.insertarFinal(new CambioAgregar(1,1,it.elemActual().segundo-1));
-        j=it.elemActual().segundo;
-    }
-    it.sucesor();
-    while (!it.terminado()) {
-        if (i+1 < it.elemActual().primero) {
-            Cambios.insertarFinal(new CambioEliminar(j,i+1,it.elemActual().primero-1));
+    if(!it.terminado()){ //Si la subsecuencia no es vacía.
+
+        if (i < it.elemActual().primero) {
+            Cambios.insertarFinal(new CambioEliminar(it.elemActual().segundo,1,it.elemActual().primero-1));
+            i=it.elemActual().primero;
         }
-        if (j+1 <it.elemActual().segundo) {
-            Cambios.insertarFinal(new CambioAgregar(i,j+1,it.elemActual().segundo-1));
+        if (j < it.elemActual().segundo) {
+            Cambios.insertarFinal(new CambioAgregar(1,1,it.elemActual().segundo-1));
+            j=it.elemActual().segundo;
         }
-        i=it.elemActual().primero;
-        j=it.elemActual().segundo;
         it.sucesor();
+        while (!it.terminado()) {
+            if (i+1 < it.elemActual().primero) {
+                Cambios.insertarFinal(new CambioEliminar(j,i+1,it.elemActual().primero-1));
+            }
+            if (j+1 <it.elemActual().segundo) {
+                Cambios.insertarFinal(new CambioAgregar(i,j+1,it.elemActual().segundo-1));
+            }
+            i=it.elemActual().primero;
+            j=it.elemActual().segundo;
+            it.sucesor();
+        }
+        if (it.elemUltimo().primero<archorig->getCantLineas())
+            Cambios.insertarFinal(new CambioEliminar(archdest->getCantLineas(),it.elemUltimo().primero+1,archorig->getCantLineas()));
+        if (it.elemUltimo().segundo<archdest->getCantLineas())
+            Cambios.insertarFinal(new CambioAgregar(archorig->getCantLineas(),it.elemUltimo().segundo+1,archdest->getCantLineas()));
     }
-    if (it.elemUltimo().primero<archorig->getCantLineas())
-        Cambios.insertarFinal(new CambioEliminar(archdest->getCantLineas(),it.elemUltimo().primero+1,archorig->getCantLineas()));
-    if (it.elemUltimo().segundo<archdest->getCantLineas())
-        Cambios.insertarFinal(new CambioAgregar(archorig->getCantLineas(),it.elemUltimo().segundo+1,archdest->getCantLineas()));
+    else // Si la subsecuencia es vacía.
+    {
+        Cambios.insertarFinal(new CambioEliminar(archdest->getCantLineas(),1,archorig->getCantLineas()));
+        Cambios.insertarFinal(new CambioAgregar(1,1,archdest->getCantLineas()));
+    }
 }
 
 void Diferencial::imprimirDiff()
