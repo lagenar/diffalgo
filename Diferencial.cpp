f9f99066e83707dd4253290e294ac132328ea190
lucas <lucas@lucas-desktop>
2009-05-27 09:01:48 -0300

    se modific? el m?todo calcularSubsecuencia. Ahora usa el algoritmo de programaci?n din?mica. Estos cambios se hicieron en la segunda rama

--- Diferencial.cpp
+++ Diferencial.cpp
@@ -11,17 +11,37 @@ Diferencial::Diferencial(Archivo & archorig, Archivo & archobj)
 
 void Diferencial::calcularSubsecuencia(Lista<parInt> & subsecuencia)
 {
-    int k = 1;
-    int j = 1;
-    for (int i = 1; i <= archorig->getCantLineas(); i++) {
-        j = k;
-        while(j <= archobj->getCantLineas() && archorig->getLinea(i) != archobj->getLinea(j))
-            j++;
-        if (j <= archobj->getCantLineas()) {
-            subsecuencia.insertarFinal(parInt(i, j));
-            k = j + 1;
+    int m = archorig->getCantLineas() + 1;
+    int n = archobj->getCantLineas() + 1;
+    int MatLCS[m][n];
+    for (int i = 1; i < m; i++)
+        MatLCS[i][0] = 0;
+    for (int i = 0; i < n; i++)
+        MatLCS[0][i] = 0;
+    for (int i = 1; i < m; i++)
+        for (int k = 1; k < n; k++) {
+            if (archorig->getLinea(i) == archobj->getLinea(k))
+                MatLCS[i][k] = MatLCS[i - 1][k - 1] + 1;
+            else if (MatLCS[i - 1][k] >= MatLCS[i][k - 1])
+                MatLCS[i][k] = MatLCS[i - 1][k];
+            else
+                MatLCS[i][k] = MatLCS[i][k - 1];
         }
-    }
+   m--;
+   n--;
+   while (m > 0 && n > 0) {
+       if (archorig->getLinea(m) == archobj->getLinea(n)) {
+           subsecuencia.insertarPrincipio(parInt(m, n));
+           m--;
+           n--;
+       }
+       else if (MatLCS[m - 1][n] >= MatLCS[m][n - 1]) {
+           m--;
+       }
+       else {
+           n--;
+       }
+   }
 }
 
 void Diferencial::calcularCambios(Lista<Cambio*> & Cambios, Lista<parInt> & subsecuencia)
