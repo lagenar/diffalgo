e538d24dc88afbcd8ee58c7ca7495e19c5ecd9aa
lucas <lucas@lucas-desktop>
2009-06-08 20:26:04 -0300

    se cambio la clase cambio y sus derivadas para que contengan una lista de lineas(que se agregan o eliminan). Esto sirve para que no se tengan que pasar par?metros de m?s para soportar el polimorfismo

--- Cambio.cpp
+++ Cambio.cpp
@@ -1,33 +1,43 @@
 #include "Cambio.h"
 
-CambioAgregar::CambioAgregar(int lineaOrigen, int lineaDestinoComienzo, int lineaDestinoFinal)
+CambioAgregar::CambioAgregar(const Archivo & objetivo, int lineaOrigen, int lineaDestinoComienzo, int lineaDestinoFinal)
 {
     this->lineaOrigen = lineaOrigen;
     this->lineaDestinoComienzo = lineaDestinoComienzo;
     this->lineaDestinoFinal = lineaDestinoFinal;
+    for (int k = lineaDestinoComienzo; k <= lineaDestinoFinal; k++)
+        lineas.insertarFinal(objetivo.getLinea(k));
 }
 
-string CambioAgregar::getDiff(const Archivo & archorig, const Archivo & archdest)
+string CambioAgregar::getDiff()
 {
     stringstream resultado; // Se usa para la conversión de int a string.
     resultado << lineaOrigen << "a" << lineaDestinoComienzo << "," << lineaDestinoFinal << endl;
-    for (int i = lineaDestinoComienzo; i <= lineaDestinoFinal; i++)
-        resultado << "> " << archdest.getLinea(i) << endl;
+    IteradorLista<string> it(&lineas);
+    while (!it.terminado()) {
+        resultado << "> " << it.elemActual() << endl;
+        it.sucesor();
+    }
     return resultado.str();
 }
 
-CambioEliminar::CambioEliminar(int lineaDestino, int lineaOrigenComienzo, int lineaOrigenFinal)
+CambioEliminar::CambioEliminar(const Archivo & origen, int lineaDestino, int lineaOrigenComienzo, int lineaOrigenFinal)
 {
     this->lineaDestino = lineaDestino;
     this->lineaOrigenComienzo = lineaOrigenComienzo;
     this->lineaOrigenFinal = lineaOrigenFinal;
+    for (int k = lineaOrigenComienzo; k <= lineaOrigenFinal; k++)
+        lineas.insertarFinal(origen.getLinea(k));
 }
 
-string CambioEliminar::getDiff(const Archivo & archorig, const Archivo & archdest)
+string CambioEliminar::getDiff()
 {
     stringstream resultado;
     resultado << lineaOrigenComienzo << "," << lineaOrigenFinal << "d" << lineaDestino << endl;
-    for (int i = lineaOrigenComienzo; i <= lineaOrigenFinal; i++)
-        resultado << "< " << archorig.getLinea(i) << endl;
+    IteradorLista<string> it(&lineas);
+    while (!it.terminado()) {
+        resultado << "< " << it.elemActual() << endl;
+        it.sucesor();
+    }
     return resultado.str();
 }
