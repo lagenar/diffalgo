58a540d98bea5b6dd2f4f9aa7a05b4031e8d8f58
lucas <lucas@lucas-desktop>
2009-06-25 19:39:20 -0300

    en las llamadas a string.find se cambio -1 por string::npos para sacar los warnings

--- diff.cpp
+++ diff.cpp
@@ -6,10 +6,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    /*Archivo Diff("/home/mmatos/Desktop/diff.diff");
-    Diferencial diferencial(Diff,true);
-    diferencial.imprimirDiff();
-    return 0;*/
     if (argc < 3) {
         cout << "uso: diff <archivo origen> <archivo objetivo>" << endl;
         return 0;
