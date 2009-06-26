c4b240878e288e48349c8e34fd7a5d3f0872676b
lucas <lucas@localhost>
2009-06-26 11:04:45 -0300

    Se agregaron destructores a las clases de cambio y a cambio porque la compilaci?n en windows daba warnings.

--- diff.cpp
+++ diff.cpp
@@ -6,11 +6,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    Archivo orig(argv[1]);
-    Archivo diff(argv[2]);
-    Diferencial diferencial(diff, orig, false);
-    diferencial.aplicarPatch();
-    return 0;
     if (argc < 3) {
         cout << "uso: diff <archivo origen> <archivo objetivo>" << endl;
         return 0;
@@ -26,9 +21,9 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-//    Subsecuencia subsec;
-//    calcularSubsecuencia(origen, objetivo, subsec);
-//    Diferencial diff(origen, objetivo, subsec);
-//    diff.imprimirDiff();
-//    return 0;
+    Subsecuencia subsec;
+    calcularSubsecuencia(origen, objetivo, subsec);
+    Diferencial diff(origen, objetivo, subsec);
+    diff.imprimirDiff();
+    return 0;
 }
