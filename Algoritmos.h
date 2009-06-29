168f9ba80e00bc8f33ddbff02fec5f97cf2978a9
lucas <lucas@lucas-desktop>
2009-06-29 17:08:42 -0300

    Se agregaron los archivos Algoritmos.h y Algoritmos.cpp a donde se pas? la funci?n calcularSubsecuencia y la clase parInt

---
+++ Algoritmos.h
@@ -0,0 +1,19 @@
+#ifndef ALGORITMOS_H
+#define ALGORITMOS_H
+#include "Archivo.h"
+#include "Lista.h"
+
+class parInt {
+    public:
+        int Primero() { return primero; }
+        int Segundo() { return segundo; }
+        parInt(int p, int s) : primero(p), segundo(s) { }
+    private:
+        int primero, segundo;
+};
+
+typedef Lista<parInt> Subsecuencia; 
+
+void calcularSubsecuencia(const Archivo &, const Archivo &, Subsecuencia &);
+
+#endif
