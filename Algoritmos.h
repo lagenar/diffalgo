d6789e8fb5a0ac2f97af5850196be02d05dd90d4
lucas <lucas@lucas-desktop>
2009-07-06 21:05:20 -0300

    Se cre? un nuevo archivo Subsecuencia.h que contiene la declaraci?n de parInt y subsecuencia est? nos permite sacar los archivos Algoritmos.h y Algoritmos.cpp del proyecto patch

--- Algoritmos.h
+++ Algoritmos.h
@@ -1,18 +1,7 @@
 #ifndef ALGORITMOS_H
 #define ALGORITMOS_H
 #include "Archivo.h"
-#include "Lista.h"
-
-class parInt {
-    public:
-        int Primero() { return primero; }
-        int Segundo() { return segundo; }
-        parInt(int p, int s) : primero(p), segundo(s) { }
-    private:
-        int primero, segundo;
-};
-
-typedef Lista<parInt> Subsecuencia;
+#include "Subsecuencia.h"
 
 void calcularSubsecuencia(const Archivo &, const Archivo &, Subsecuencia &);
 
