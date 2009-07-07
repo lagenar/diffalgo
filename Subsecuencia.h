eb0ee9613b1a232aeb0eb8e779c99bb1a41cadb5
lucas <lucas@lucas-desktop>
2009-07-06 21:07:04 -0300

    me hab?a olvidado de agregar subsecuencia.h

---
+++ Subsecuencia.h
@@ -0,0 +1,17 @@
+#ifndef SUBSECUENCIA_H
+#define SUBSECUENCIA_H
+#include "Lista.h"
+
+class parInt {
+    public:
+        int Primero() { return primero; }
+        int Segundo() { return segundo; }
+        parInt(int p, int s) : primero(p), segundo(s) { }
+    private:
+        int primero, segundo;
+};
+
+typedef Lista<parInt> Subsecuencia;
+
+
+#endif // SUBSECUENCIA_H
